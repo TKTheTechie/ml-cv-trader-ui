84d8e4610cc398b62f3a7e6959b49b88aaef5f6c
TKTheTechie <thomas.kunnumpurath@solace.com>
2022-02-28 08:37:06 -0500

    Update Header.svelte

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -9,7 +9,7 @@
       <span class="relative header-text z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-green-500 select-none">ml-cv-trader</span>
 
       <nav class="top-0 left-0 z-0 flex items-center justify-center w-full h-full py-5 -ml-0 space-x-5 text-base md:-ml-5 md:py-0 md:absolute">
-        <a href="https://github.com/TKTheTechie/cv-ml-trader-ui" class="relative text-lg leading-6 dark:text-white transition duration-150 ease-out hover:text-slate-500" target="_blank">
+        <a href="https://github.com/TKTheTechie/ml-cv-trader-ui" class="relative text-lg leading-6 dark:text-white transition duration-150 ease-out hover:text-slate-500" target="_blank">
           <Icon data={github} scale={2} />
         </a>
       </nav>
